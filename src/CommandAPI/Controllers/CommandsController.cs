aa5c4a2703c5bcd651117d5ec083e6e199529701
Leila Yang <ava_yangli@163.com>
2020-12-10 07:38:32 +0000

    Added Model and Mock Repository

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Data;
+using CommandAPI.Models;
 
 namespace CommandAPI.Controllers
 {
@@ -7,11 +9,39 @@ namespace CommandAPI.Controllers
     [ApiController]
     public class CommandsController:ControllerBase
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string [] {"this","is","hard","coded"};
+        private readonly ICommandAPIRepo _repository; //cant new a Interface
 
+        /*when the constructor is called, the DI system will inject the required dependency
+        when we ask for an instance of ICommandAPIRepo*/
+        public CommandsController(ICommandAPIRepo repository) //Constructor with input interface
+        {
+            _repository=repository;// be assigned the injected MockCommandAPIRepe 
+            //repository is the injected dependency (in this case MockCommandAPIRepo)
+        }
+
+
+        // [HttpGet]
+        // public ActionResult<IEnumerable<string>> Get()
+        // {
+        //     return new string [] {"this","is","hard","coded"};
+
+        // }
+        [HttpGet] //reponds to GET verb
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllCommands();
+            return Ok(commandItems); //Return a HTTP 200 Result (OK) and pass back our result set
+        }
+        
+        [HttpGet("{id}")] 
+        public ActionResult<IEnumerable<Command>> GetAllCommandsById(int Id)
+        {
+            var commandItem = _repository.GetCommandById(Id);
+            if (commandItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(commandItem); 
         }
     }
 }
--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CommandAPI.Data;
 
 namespace CommandAPI
 {
@@ -17,6 +18,8 @@ namespace CommandAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();//registers sercices to enable the use of Controllers
+
+            services.AddScoped <ICommandAPIRepo,MockCommandAPIRepo>();// register IcommandAPIRepo and Mock...
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
